8a3416ed73e021af5abf5cd64875cccf32ffcca6
Marcin Bielak <marcin.bieli@gmail.com>
2014-02-27 23:26:48 +0100

    compiled service libs prototypes

--- src/main/net/bieli/services/DnsLookup.java
+++ src/main/net/bieli/services/DnsLookup.java
@@ -1,8 +1,28 @@
 package net.bieli.services;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 public final class DnsLookup {
-  public String getHostName(String ipAddress) {
-    return "";
+  public String getHostName(String ipAddress) throws UnknownHostException  {
+	String hostName = new String("");    
+
+	    try {
+	      InetAddress host;
+	 
+		host = InetAddress.getByName(ipAddress);
+
+	      if (!host.getHostAddress().equals(host.getHostName())) {
+		//System.out.println(">>>>>> IP: " + host.getHostAddress());
+		//System.out.println("-- HOST: " + host.getHostName());
+		hostName = host.getHostName();
+	      }
+	    } catch (UnknownHostException e) {
+		hostName = "";	      
+		e.printStackTrace();
+	    }
+
+	return hostName;
   }
 }
 
--- src/main/net/bieli/services/HttpHeader.java
+++ src/main/net/bieli/services/HttpHeader.java
@@ -1,8 +1,58 @@
 package net.bieli.services;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public final class HttpHeader {
-  public String getRawHeaders(String url) {
-    return "";
-  }
+  private final static int PORT = 80;
+  private final static String USER_AGENT = "User-Agent: Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:27.0) Gecko/20100101 Firefox/27.0";
+ 
+  public String getRawHeaders(String hostname) throws IOException {
+	String output = new String("");        
+	Socket socket = null;
+        PrintWriter writer = null;
+        BufferedReader reader = null;
+
+        try {
+            socket = new Socket(hostname, PORT);
+            writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream()));
+
+            writer.println("GET / HTTP/1.1");
+            writer.println("Host: " + hostname);
+            writer.println("Accept: */*");
+            writer.println(USER_AGENT);
+            writer.println(""); // Important, else the server will expect that there's more into the request.
+            writer.flush();
+
+            reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            for (String line; (line = reader.readLine()) != null;) {
+                if (line.isEmpty()) {
+			break; // Stop when headers are completed. We're not interested in all the HTML.
+		}
+		output.concat(line); 
+                //System.out.println(line);
+            }
+        } finally {
+            if (reader != null) {
+		try { 
+			reader.close();
+		} catch (IOException logOrIgnore) { };
+	    } 
+            if (writer != null) {
+		writer.close();
+	    }
+            if (socket != null) {
+		try {
+			socket.close();
+		} catch (IOException logOrIgnore) { };
+	    } 
+        }
+
+	return output;
+    }
 }
 
